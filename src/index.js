654ad31527bada02350027485a211544c151ccb5
ConstantineTU <8kozmo8@gmail.com>
2021-08-25 19:23:49 +0500

    init: task expression-calculator complete

--- src/index.js
+++ src/index.js
@@ -4,7 +4,14 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    let opBracket = 0
+    let clBracket = 0
+    if (expr.indexOf('/ 0') + 1) { throw new Error('TypeError: Division by zero.') }
+    for (let i = 0; i < expr.length; i++) {
+        if (expr[i] === '(') { opBracket++ }
+        if (expr[i] === ')') { clBracket++ }
+    }
+    if (opBracket !== clBracket) { throw new Error('ExpressionError: Brackets must be paired') }
 }
 
 module.exports = {
